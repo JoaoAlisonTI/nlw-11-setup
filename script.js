0433c09694276dc249357b30707a44d68b830727
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-01-22 17:39:22 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('[João Alison 👨🏽‍💻] App de Hábitos');
+
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+  
+  alert('Adicionado com sucesso ☑️')
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem('nlw11Setup@alison', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("nlw11Setup@alison")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
